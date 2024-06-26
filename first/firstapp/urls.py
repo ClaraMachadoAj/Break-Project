e252ae5e4fc01d981003bc3ec3b8551a6c0270c6
Clara Machado <claramachado2005@gmail.com>
2024-06-26 12:05:23 -0300

    ihuu

--- first/firstapp/urls.py
+++ first/firstapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'firstapp'
 
 urlpatterns = [
-    path("", views.AlbumView, name = "index")
+    path("", views.AlbumView.as_view(), name = "index"),
+    path("album/<int:id>/", views.AlbumViewOne.as_view(), name = "album_detail")
 ]
--- first/firstapp/views.py
+++ first/firstapp/views.py
@@ -7,7 +7,11 @@ class AlbumView(View):
     def get(self, request):
         ultimos_albuns =  CadastroAlbums.obter_ultimos_albuns(10)
         context = {'albuns': ultimos_albuns}
-        return render(request, 'first/index.html', context)
-    
+        return render(request, 'index.html', context)
     
 
+class AlbumViewOne(View):
+    def get(self, request, id):
+        album = CadastroAlbums.obter(id)
+        context = {'album': album}
+        return render(request, 'album_detail.html', context)
